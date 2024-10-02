210b5c73d558b992253938ebd6dd52b8fb21a8d1
bhardwajdevesh092005 <b23cs1014@iitj.ac.in>
2024-10-03 01:42:09 +0530

    Setup Done:

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+function App() {
+  return (
+    <div className='bg-black flex justify-center w-screen h-screen text-white'>
+        <div className='flex flex-col justify-center text-3xl text-yellow-400'>
+          DSA Project Frontend
+        </div>
+    </div>
+  )
+}
+
+export default App
