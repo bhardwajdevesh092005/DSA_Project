40104b6b2ac5a1c9a6a4588a47e76e34c4a90c41
diwanshuydv <b23cs1017@iitj.ac.in>
2024-10-12 15:55:30 +0530

    added file to read from .geojson format and return JSONObject

---
+++ API Folder/src/main/java/com/example/demo/Algorithm/GeoJSONParser.java
@@ -0,0 +1,26 @@
+package com.example.demo.Algorithm;
+
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+public class GeoJSONParser {
+    public static JsonObject parseGeoJSON(String filePath) {
+        String geoJsonData = readFile(filePath);
+        Gson gson = new Gson();
+        return gson.fromJson(geoJsonData, JsonObject.class);
+    }
+    
+    public static String readFile(String filePath) {
+        try {
+            return new String(Files.readAllBytes(Paths.get(filePath)));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
